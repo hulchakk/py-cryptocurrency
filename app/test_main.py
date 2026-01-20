031f16090dae94865fe2ee28f36807829660ac13
Dmytro <ys33ys33ys55@gmail.com>
2026-01-20 17:23:19 +0100

    Solution fixed all problems

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,34 @@
-from unittest import mock
-
+from unittest.mock import patch, MagicMock
 from app.main import cryptocurrency_action
 
 
-@mock.patch("app.main.get_exchange_rate_prediction")
-def test_func_returns_correct_values1(
-    mocked_func: mock.MagicMock
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_func_returns_correct_values(
+    mocked_func: MagicMock
 ) -> None:
-    mocked_func.return_value = 100
-    assert cryptocurrency_action(95) == "Buy more cryptocurrency"
-    assert cryptocurrency_action(106) == "Sell all your cryptocurrency"
+    mocked_func.return_value = 94.9
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_func_returns_correct_values1(
+    mocked_func: MagicMock
+) -> None:
+    mocked_func.return_value = 105.001
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+    
+@patch("app.main.get_exchange_rate_prediction")
+def test_func_returns_correct_values2(
+    mocked_func: MagicMock
+) -> None:
+    mocked_func.return_value = 95
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_func_returns_correct_values3(
+    mocked_func: MagicMock
+) -> None:
+    mocked_func.return_value = 105
     assert cryptocurrency_action(100) == "Do nothing"
