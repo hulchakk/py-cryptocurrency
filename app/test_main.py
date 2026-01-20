3d3ed60b067fae83542c61e8f66b41e7859fbb69
Dmytro <ys33ys33ys55@gmail.com>
2026-01-20 16:08:49 +0100

    Solution 3

--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,29 @@
 import pytest
 from unittest import mock
-from typing import Any, Callable
 
 from app.main import cryptocurrency_action
 
+
 @pytest.fixture()
-def mocked_func():
+def mocked_func() -> mock.MagicMock:
     with mock.patch("app.main.get_exchange_rate_prediction") as func:
         func.return_value = 50
         yield func
 
+
 def test_func_returns_correct_values1(
-    mocked_func
+    mocked_func: mock.MagicMock
 ) -> None:
     assert cryptocurrency_action(45) == "Do nothing"
 
 
 def test_func_returns_correct_values2(
-    mocked_func
+    mocked_func: mock.MagicMock
 ) -> None:
     assert cryptocurrency_action(44.9) == "Buy more cryptocurrency"
 
 
 def test_func_returns_correct_values3(
-    mocked_func
+    mocked_func: mock.MagicMock
 ) -> None:
     assert cryptocurrency_action(55.1) == "Sell all your cryptocurrency"
-
